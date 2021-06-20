e31434a6fd9d8ae06d71604c410991c4c1ddd36e
TomeR <tomerrub11@gmail.com>
2021-06-20 16:54:33 +0300

    Webserver can now receive data from the client and re-render it each 5 seconds(can be changed) + Added AJAX call to the /getData route to get data from the database

--- DB_Utils.py
+++ DB_Utils.py
@@ -3,6 +3,9 @@ from app import app
 
 db = SQLAlchemy(app)
 
+# TODO: When the client disconnects from the webserver, delete him from the db.
+# ^ solution: do it in the frontend, if no update in 15 seconds, remove from db and acknowledge the frontend.
+
 class Client(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user = db.Column(db.String(20), nullable=False)
@@ -10,24 +13,26 @@ class Client(db.Model):
   cpuUsage = db.Column(db.String(5), nullable=False)
   memory = db.Column(db.String(20), nullable=False)
   procs = db.Column(db.String(200))
-  ram = db.Column(db.String(20))
 
-  """
-    If client doesn't exists, create a new field.
-    If it does, update the right field.
-  """
   def addClient(self):
-
+    # Update values for user if the user exists on the db
+    if Client.query.filter_by(user=self.user).first() is not None:
+      print(f"Not none: {self.cpuUsage} {self.memory} {self.user}")
+      _user = Client.query.filter_by(user=self.user).first()
+      _user.cpuUsage = self.cpuUsage
+      _user.memory = self.memory
+      db.session.commit()
+      return
+    
     c = Client(
       user=self.user, 
       cpuType=self.cpuType,
       cpuUsage=self.cpuUsage,
       memory=self.memory,
       procs=self.procs,
-      ram=self.ram
     )
     db.session.add(c)
     db.session.commit()
 
   def __repr__(self):
-    return f"Client({self.id}, {self.user}, {self.cpuType})"
+    return f"Client({self.id}, {self.user}, {self.cpuUsage}, {self.memory})"
--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, request, jsonify, Response
-from DB_Utils import Client
+from DB_Utils import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # TODO: In production, change to a postgres db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+isAuthed = False # WARNING: global variables are not allowed!
 
 @app.route("/", methods=["GET"])
 def homepage():
-  return render_template("pages/DashboardPage.html") # was: pages/Homepage.html
+  return render_template("pages/Homepage.html") # was: pages/DashboardPage.html
 
 @app.route("/", methods=["POST", "GET"])
 def homepage_post():
+  global isAuthed
   username = request.form['username']
   password = request.form['password']
+
   if username == "root" and password == "toor":
+    isAuthed = True
     return render_template("pages/DashboardPage.html")
+  
   return render_template("pages/Homepage.html", error=True)
 
-# TODO: Create a function to handle API calls and store the info at
-# the clients dictionary.
 @app.route("/api", methods=["POST"])
 def api():
   cpuType = request.form.get('cpuType')
@@ -36,7 +40,6 @@ def api():
     cpuUsage=cpuUsage,
     memory=memory,
     procs=procs,
-    ram=ram
   )
   client.addClient()
 
@@ -50,5 +53,16 @@ def api():
     "processes": procs,
   })
 
+
+@app.route("/getData", methods=["GET"])
+def getDataEndpoint():
+  global isAuthed
+  if not isAuthed:
+    return "Sorry, you seem to have no permission to visit this page :("
+
+  clients = [(i.user, i.cpuType, i.cpuUsage, i.memory, i.procs) for i in Client.query.all()]
+  return jsonify(clients=clients)
+
 if __name__ == "__main__":
+  db.init_app(app)
   app.run(debug=True, threaded=True)
